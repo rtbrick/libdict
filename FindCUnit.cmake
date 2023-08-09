2223b9b6909fff5f6dc09711492f85a5a7797048
Christian Giese <christian@rtbrick.com>
2023-08-09 14:33:17 +0000

    fix cmake files

--- FindCUnit.cmake
+++ FindCUnit.cmake
@@ -2,8 +2,8 @@
 # CUNIT_LIBRARIES - List of libraries when using libcunit.
 # CUNIT_FOUND - True if libcunit found.
 if(CUNIT_INCLUDE_DIRS)
-	# Already in cache, be silent
-	set(CUNIT_FIND_QUIETLY YES)
+    # Already in cache, be silent
+    set(CUNIT_FIND_QUIETLY YES)
 endif()
 
 find_path(CUNIT_INCLUDE_DIRS CUnit/CUnit.h)
@@ -12,8 +12,8 @@ find_library(CUNIT_LIBRARY NAMES cunit libcunit)
 # handle the QUIETLY and REQUIRED arguments and set CUNIT_FOUND to TRUE if
 # all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(CUNIT DEFAULT_MSG CUNIT_LIBRARY CUNIT_INCLUDE_DIRS)
+find_package_handle_standard_args(CUnit DEFAULT_MSG CUNIT_LIBRARY CUNIT_INCLUDE_DIRS)
 
 if(CUNIT_FOUND)
-	set(CUNIT_LIBRARIES ${CUNIT_LIBRARY})
+    set(CUNIT_LIBRARIES ${CUNIT_LIBRARY})
 endif()
